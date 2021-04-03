b505fab05c7374cf63299cfde264d825d5c79eff
david-rgb <david.adamlje@gmail.com>
2021-04-04 00:39:35 +0200

    Second commit

---
+++ pythonProject/main.py
@@ -0,0 +1,27 @@
+happy = "It's great to see you happy"
+nervous = "Take a deep breath 3 times"
+sad = "Go get drunk"
+excited =  "Take a chill pill, or did you take another kind of pill?"
+relaxed = "You should slow down on the pot"
+mood = sad
+
+if(mood == happy): print(happy)
+elif(mood == nervous): print(nervous)
+elif(mood == sad): print(sad)
+elif(mood == excited): print(excited)
+elif(mood == relaxed): print(relaxed)
+
+secret = 20
+guess = 19
+
+if(secret==guess): print("Congratulations, you win a dollar")
+elif(secret != guess): print("Bad luck, you should try again")
+
+num1 = 527445
+num2 = 96453
+operatation = "*"
+
+if operatation == "+": print (num1+num2)
+elif operatation == "-": print(num1-num2)
+elif operatation == "/": print(num1/num2)
+elif operatation == "*": print(num1*num2)
